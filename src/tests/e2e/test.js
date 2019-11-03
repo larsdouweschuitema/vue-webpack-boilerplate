e03f7394c1d9986b645517a5589fef39c2252c68
Lars Douwe Schuitema <info@larsschuitema.nl>
2019-11-04 00:45:45 +0100

    remove unused variable

--- src/tests/e2e/test.js
+++ src/tests/e2e/test.js
@@ -1,7 +1,5 @@
 module.exports = {
   'example e2e test': function (browser) {
-    const devServer = browser.globals.devServerURL
-
     browser
       .url('localhost:8080')
       .waitForElementVisible('div#app', 1000)
